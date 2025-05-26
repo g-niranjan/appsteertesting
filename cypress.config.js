a90e6083d65b21ce8783909cbead75c8ea56444e
g-niranjan <niranjan.g@isteer.com>
2025-05-26 12:24:23 +0530

    login application and dashboard username

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://qa.appsteer.io/#/login",
     experimentalStudio: true,
     expermentalSessionAndOrigin : true,
     setupNodeEvents(on, config) {
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,49 +1,64 @@
 /// <reference types ="cypress"/>
 describe("Login page testing in appester portal", () => {
   beforeEach("Login page testing", () => {
-    cy.visit("/");
+    cy.visit("https://qa.appsteer.io/#/login");
     cy.url().should("include", "https://qa.appsteer.io/");
   });
-    // it('apllication should be launched', () => {
-    //     cy.visit("/");
-    // });
+  // it('apllication should be launched', () => {
 
-  it("validate text in login page", () => {
-    cy.get('[class="support"]').eq(0).should(
-      "have.text",
-      "Login using Appsteer Identity"
-    );
-    cy.get('[class="support"]')
-      .should("be.visible")
-      .and("have.css", "font-size", "14.08px")
-      .and("have.css", "color", "rgba(49, 49, 49, 0.7)");
-  });
+  // });
 
-  it("validate username field should be visible and enabled", () => {
-    cy.get('input[placeholder="Username"]')
-      .should("be.visible")
-      .and("not.be.disabled");
-  });
-  it("validate username field should enter the value", () => {
-    cy.get('input[placeholder="Username"]').type("UserTest123@qa.appsteer.io");
-  });
+  // it("validate text in login page", () => {
+  //   cy.visit("https://qa.appsteer.io/#/login");
+  //   cy.get('[class="support"]')
+  //     .eq(0)
+  //     .should("have.text", "Login using Appsteer Identity");
+  //   cy.get('[class="support"]')
+  //     .should("be.visible")
+  //     .and("have.css", "font-size", "14.08px")
+  //     .and("have.css", "color", "rgba(49, 49, 49, 0.7)");
+  // });
 
-  it("validate password field should be visible and enabled", () => {
-    cy.get('input[placeholder="Password"]')
-      .should("be.visible")
-      .and("not.be.disabled");
-    cy.get('input[placeholder="Password"]').should("be.visible");
-  });
-  it("validate password field should enter the value", () => {
+  // it("validate username field should be visible and enabled", () => {
+  //   cy.get('input[placeholder="Username"]')
+  //     .should("be.visible")
+  //     .and("not.be.disabled");
+  // });
+  // it("validate username field should enter the value", () => {
+  //   cy.get('input[placeholder="Username"]').type("UserTest123@qa.appsteer.io");
+  // });
+
+  // it("validate password field should be visible and enabled", () => {
+  //   cy.get('input[placeholder="Password"]')
+  //     .should("be.visible")
+  //     .and("not.be.disabled");
+  //   cy.get('input[placeholder="Password"]').should("be.visible");
+  // });
+  // it("validate password field should enter the value", () => {
+  //   cy.get('input[placeholder="Password"]').type("Admin@123");
+  // });
+
+  it("login button should be visible and enabled", () => {
+    cy.get(
+      'input[placeholder="Username"]'
+    ).type("UserTest123@qa.appsteer.io");
     cy.get('input[placeholder="Password"]').type("Admin@123");
+    cy.get('[class="p-button-label"]').should("have.text", "LOGIN").click();
+
+    cy.url().should("include", "https://qa.appsteer.io/user/#/u/menuforms/11542");
+    cy.get("[class='breadcrumb']").should("have.text", "Rk");
+
   });
-
- it.only("login button should be visible and enabled", () => {
-    cy.get('[class="p-inputtext p-component p-element input-md ng-untouched ng-pristine ng-invalid"]').type("UserTest123@qa.appsteer.io");
-    cy.get('input[placeholder="Password"]').type("Admin@123");
-    cy.get('[class="p-button-label"]').should('have.text','LOGIN')  });
-    //cy.url().should("contains", "/user/#/u/menuforms");
-
+  
+  //cy.url().should("contains", "/user/#/u/menuforms");
+  //cy.wait(5000);
+  // cy.visit(
+  //   "https://qa.appsteer.io/user/#/u/menuforms/11542"
+  // );
+  // cy.get(".flex-m height-100 ng-star-inserted").should(
+  //   "have.text",
+  //   "Rk"
+  // );
   //   it("google authentication checking", () => {
   //     cy.visit(
   //       "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=285004993930-b7g7lkmvsfdgs4trat5pabsda2uum84v.apps.googleusercontent.com&response_type=code&scope=openid%20email&redirect_uri=https%3A%2F%2Fcloud.appsteer.io%2Fservices%2Fweb%2Flogin%2Fsecured%2Fredirect%2Fsso&state=a6374ca9-6bc0-468e-a595-9debdc266a44&access_type=offline&approval_prompt=force&service=lso&o2v=1&flowName=GeneralOAuthFlow"
@@ -75,13 +90,13 @@ describe("Login page testing in appester portal", () => {
   //     );
   //   });
 
-  it("appsteer dashboard testing", () => {
-    cy.visit(
-      "https://qa.appsteer.io/user/#/u/menuforms/11542"
-    );
-    cy.get(".flex-m height-100 ng-star-inserted").should(
-      "have.text",
-      "Rk"
-    );
-  });
+  // it("appsteer dashboard testing", () => {
+  //   cy.visit(
+  //     "https://qa.appsteer.io/user/#/u/menuforms/11542"
+  //   );
+  //   cy.get(".flex-m height-100 ng-star-inserted").should(
+  //     "have.text",
+  //     "Rk"
+  //   );
+  // });
 });
